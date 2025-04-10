828668b8db964af0707829428cb3272e3c7ef0fb
Nitin John Jude <nitin.john.jude@gmail.com>
2025-04-10 11:37:16 +0530

    Load secret key from environment variable

--- attrition/settings.py
+++ attrition/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u-q@7e*xyj*qz@(i_)(cf*8r(yph*d$fheq$mbs(=5*!tios_2'
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-u-q@7e*xyj*qz@(i_)(cf*8r(yph*d$fheq$mbs(=5*!tios_2')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
